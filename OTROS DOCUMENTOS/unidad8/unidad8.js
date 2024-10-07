42c5014129ca319f1c800172ee5c97ef29bd3971
mgabrielsuarez <146495210+mgabrielsuarez@users.noreply.github.com>
2024-10-07 11:39:49 -0300

    Add files via upload

---
+++ OTROS DOCUMENTOS/unidad8/unidad8.js
@@ -0,0 +1,17 @@
+var nombre = prompt('Ingrese su nombre o Razon social')
+alert('Hola '+ nombre)
+var edad = prompt('¿Que edad tenes?')
+var domicilio = prompt ('¿Cual es tu domicilio?')
+var compos = prompt ('¿Ya sabes como funcionan las composteras?')
+var sumarte = prompt (' ¿Te interesa aplicar nuevos habitos en tu dia a dia?')
+var dato = prompt ('¿Como te enteraste de nosotros?')
+
+
+/*
+document.write('<p> Cliente: ' + nombre +'</p>')
+                document.write('<p> Edad: ' + edad +'</p>')
+                document.write('<p> Domicilio: ' + domicilio +'</p>')
+                document.write('<p> ¿Ya Conocias las composteras?: ' + nombre +'</p>')
+                document.write('<p> ¿Queres sumarte?: ' + sumarte +'</p>')
+                document.write('<p> ¿Como nos encontraste?: ' + dato +'</p>')
+                */
