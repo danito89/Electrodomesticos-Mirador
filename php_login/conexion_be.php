1d72abfbba18ba0421ccfff3ac8eb7c6ea598356
mgabrielsuarez <mgabrielsuarez.1993@gmail.com>
2024-10-21 21:21:42 -0300

    LO LOGRE !

--- php_login/conexion_be.php
+++ php_login/conexion_be.php
@@ -2,11 +2,11 @@
 
     $conexion = mysqli_connect("localhost", "root", "", "login_register_db");
 
-     if($conexion){
-         echo "conectado exitosamente a la base de datos";
+    //  if($conexion){
+    //      echo "conectado exitosamente a la base de datos";
 
-    } else {
-        echo " no se pudo conectar a la base de datos";
-     }
+    // } else {
+    //     echo " no se pudo conectar a la base de datos";
+    //  }
 
 ?>
